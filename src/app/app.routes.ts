9c41e2b2d9194e481cfd39541676170cac6b1f8c
juan-david-restrepo <dayz2006@gmail.com>
2025-11-04 17:49:52 -0500

    se agregaron nuevas carpetas de funcionamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
+import { Home } from './components/home/home';
 import { Nav } from './shared/nav/nav';
 import { Login } from './components/login/login';
+import { Recuperar } from './components/recuperar/recuperar';
+import { Registro } from './components/registro/registro';
 
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'login', component: Login},
     {path: 'nav', component: Nav},
+    {path: 'recuperar', component: Recuperar},
+    {path: 'registro', component: Registro},
 ];
--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Registro } from '../registro/registro';
+import { Recuperar } from '../recuperar/recuperar';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink, CommonModule, Registro, Recuperar],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
