b82d1b7c5c56bc342561ca558d5a2381f5992e44
juan-david-restrepo <dayz2006@gmail.com>
2025-11-05 12:47:04 -0500

    agrego el login y las rutas

--- src/app/components/recuperar/recuperar.ts
+++ src/app/components/recuperar/recuperar.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recuperar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './recuperar.html',
   styleUrl: './recuperar.css',
 })
