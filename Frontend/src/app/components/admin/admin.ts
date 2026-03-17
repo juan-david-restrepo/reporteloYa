24e1c8fab8cbba840cc615bbf422f9a40e8f1c54
miguelrt29 <miguelrt2903@gmail.com>
2026-03-17 16:11:13 -0500

    opcion de tamaño de letra y elimino daltonismo

--- Frontend/src/app/components/admin/admin.ts
+++ Frontend/src/app/components/admin/admin.ts
@@ -56,11 +56,10 @@ export class Admin implements OnInit, AfterViewInit, OnDestroy {
     const isDark = localStorage.getItem('darkMode') === 'true';
     if (isDark) document.body.classList.add('dark-mode');
 
-    const isColorBlind = localStorage.getItem('colorBlind') === 'true';
-    if (isColorBlind) document.body.classList.add('color-blind');
-
-    const savedSize = localStorage.getItem('fontSize') || 'normal';
-    document.body.classList.add(`font-${savedSize}`);
+    const savedSize = localStorage.getItem('fontSize');
+    if (savedSize) {
+      document.body.style.setProperty('--admin-font-size', savedSize + 'px');
+    }
   }
 
   ngOnInit(): void {
